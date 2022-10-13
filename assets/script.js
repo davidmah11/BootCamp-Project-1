83f866f8b3cd8e31b4e6bbda651c4334d8b68ed7
mattmald <Mat.maldonado012@gmail.com>
2022-10-13 18:45:57 -0500

    more JS

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,19 @@ var CampSerchAPI = "9EN8WYGJV7355PP4SB2VCKTF";
 var ActivityScerchAPI = "KRQZWDV5975JT6JSUC25GAM7";
 var campSites;
 
-var button = $(".submit").on('click', fetch)
+//  var button = $(".pure-button").on('click', fetchWeather())
+$(document).ready(function() {
+  $('.pure-button').click(function() {
+    var name = $(".pure-input-rounded").val();
+    console.log(name);
+  })
 
-fetch('http://api.amp.active.com/search?{campSites}&api_key={CampSerchAPI}', {
+// fetchWeather(name);
+});
+
+function fetchWeather (name) {
+ // console.log(name);
+fetch('http://api.amp.active.com/search?' + {name} + '&api_key=' + {CampSerchAPI}, {
   method: 'GET',
   credentials: 'same-origin', 
   redirect: 'follow', 
@@ -15,5 +25,15 @@ fetch('http://api.amp.active.com/search?{campSites}&api_key={CampSerchAPI}', {
   .then(function (data) {
     console.log(data);
   });
+}
+//doesn't work 
+//doesn't work
+//doesn't work
 
-  //doesn't work 
+  // type in a campground name 
+  // hit search 
+  // campground data appears
+
+  // create an input box and link it to javascript
+  // on click i need to get the campground name
+  // now i can put the name in my API and go fetch the data 
