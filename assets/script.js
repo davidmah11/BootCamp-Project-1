d3bd4dfac742a1c01bdbe635fce52430fc536bb4
mattmald <Mat.maldonado012@gmail.com>
2022-10-13 21:10:52 -0500

    update

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,9 @@
 //Works! may need to make a drop down menu for all states?
 //CampSerchAPI = "u5zcb98zk5zgfqvnp47tyc46"
 
-<<<<<<< HEAD
-//  var button = $(".pure-button").on('click', fetchWeather())
-$(document).ready(function() {
-  $('.pure-button').click(function() {
-    var name = $(".pure-input-rounded").val();
-    console.log(name);
-  })
-
-// fetchWeather(name);
-});
-
-function fetchWeather (name) {
- // console.log(name);
-fetch('http://api.amp.active.com/search?' + {name} + '&api_key=' + {CampSerchAPI}, {
-=======
 document.getElementById("submit").addEventListener("click", fetch)
 
-/*fetch('http://api.amp.active.com/camping/campgrounds/?pstate=MN&siteType=2001&hookup=3002&sewer=3007&pets=3010&api_key=u5zcb98zk5zgfqvnp47tyc46', {
->>>>>>> 1210028998c3d704bb056cc6c916e8aba386e810
+fetch('http://api.amp.active.com/camping/campgrounds/?pstate=MN&siteType=2001&hookup=3002&sewer=3007&pets=3010&api_key=u5zcb98zk5zgfqvnp47tyc46', {
   method: 'GET',
   mode: "no-cors",
  
@@ -30,23 +14,9 @@ document.getElementById("submit").addEventListener("click", fetch)
   .then(function (data) {
     console.log(data);
   });
-<<<<<<< HEAD
-}
-//doesn't work 
-//doesn't work
-//doesn't work
 
-  // type in a campground name 
-  // hit search 
-  // campground data appears
-
-  // create an input box and link it to javascript
-  // on click i need to get the campground name
-  // now i can put the name in my API and go fetch the data 
-=======
-*/
   //google maps API AIzaSyC9_TkzM_-uFH6tjC-d7T0k7YcSOmaCYus
-/*
+
   fetch("https://maps.googleapis.com/maps/api/js?key=AIzaSyCUTcG2fB7T8jhOgj3Y780NCbeatPkvwgUus&callback=initMap", {
   method: 'GET',
   mode: "no-cors"
@@ -54,7 +24,7 @@ document.getElementById("submit").addEventListener("click", fetch)
 .then(function (data) {
   console.log(data);
 });
-*/
+
 //Think this is for google map with marker for location?
 // Initialize and add the map
 function initMap() {
@@ -63,7 +33,7 @@ function initMap() {
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
-    center: uluru,
+    center: {lat: 0, lng:0},
   });
   // The marker, positioned at Uluru
   const marker = new google.maps.Marker({
@@ -71,6 +41,34 @@ function initMap() {
     map: map,
   });
 }
+// =====================================================
+
+var map = new googleMaps.Map(document.getElementById('map'), {
+  zoom: 12,
+  center: {lat: 0,  lng:  0}
+});
+
+marker = new googleMaps.Marker({
+  map:map,
+  draggable:true,
+  animation: google.maps.Animation.DROP,
+  position: {lat: 0,  lng:  0}
+});
+var onChangeHandler = function() {
+DisplayPoint(map);
+};
+document.getElementById('start').addEventListener('change', onChangeHandler);
+document.getElementById('end').addEventListener('change', onChangeHandler);
+
+
+function DisplayPoint(map) {
+
+var lat = document.getElementById('start').value;
+var lng = document.getElementById('end').value;
+var latLng = new google.maps.LatLng(lat, lng);
+marker.setPosition(latLng);
+map.panTo(latLng);
+
+}
 
 window.initMap = initMap;
->>>>>>> 1210028998c3d704bb056cc6c916e8aba386e810
