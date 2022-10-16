04602e436939f37ad4e05cd9e24bc279897cc919
Chad Kostohryz <kostc7@gmail.com>
2022-10-16 16:58:43 -0500

    hard code search state city zip works

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ CampSerchAPI = "d99a2fc1-ab7f-4aa9-840f-4ea2e3a166a0"
 
 document.getElementById("submit").addEventListener("click", fetch)
 
-fetch('https://ridb.recreation.gov/api/v1/campsites?limit=50&offset=0&apikey='+CampSerchAPI, {
+fetch('https://ridb.recreation.gov/api/v1/facilityaddresses?query=minnesota&limit=10&offset=0&apikey='+CampSerchAPI, {
   method: 'GET',
   headers: {
     accept: 'application/json'
@@ -16,34 +16,10 @@ fetch('https://ridb.recreation.gov/api/v1/campsites?limit=50&offset=0&apikey='+C
     console.log(data['RECDATA']);
 })
 
-//need to do something on a page 
-/*fetch('', {
-  method: 'GET',
-  mode: "no-cors",
- 
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
-*/
-  //google maps API AIzaSyC9_TkzM_-uFH6tjC-d7T0k7YcSOmaCYus
-/*
-  fetch("https://maps.googleapis.com/maps/api/js?key=AIzaSyCUTcG2fB7T8jhOgj3Y780NCbeatPkvwgUus&callback=initMap", {
-  method: 'GET',
-  mode: "no-cors"
-})
-.then(function (data) {
-  console.log(data);
-});
-*/
-//Think this is for google map with marker for location?
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
+  const uluru = { lat: 30.39777709, lng: -88.796144988 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
